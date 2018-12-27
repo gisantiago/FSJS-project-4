55913224eb2cba581112435874b5e9d6190d3357
Gabriel Santiago <gisantiago@live.com>
2018-12-27 12:58:24 -0500

    Added Game and Phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,39 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrases = [
+            new Phrase("A bird in the hand is worth two in the bush"),
+            new Phrase("Busy as a bee"),
+            new Phrase("As happy as Larry"),
+            new Phrase("kill two birds with one stone"),
+            new Phrase("Bring home the bacon")
+        ];
+
+        return phrases;
+    };
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const randPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randPhrase;
+    };
+
+ }
+
+ 
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const phrase = this.phrase.split('');
+        
+        for(var i = 0; i < phrase.length; i++) {
+            if(phrase[i] !== " ") {
+              var li = `<li class="letter">${phrase[i]}</li>`;
+              document.querySelector('#phrase ul').append(li);  
+            } else {
+                var li = `<li class="space">${phrase[i]}</li>`;
+                document.querySelector('#phrase ul').append(li); 
+            }
+        }
+        console.log(phrase); // for testing purposes...
+    }
+
+     checkLetter() {
+
+     }
+
+     showMatchedLetter() {
+         
+     }
+ }
