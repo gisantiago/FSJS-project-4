6d64c9d25859cb7a999f7a414b982cf524d6135d
Gabriel Santiago <gisantiago@live.com>
2018-12-27 14:49:12 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * app.js */
+* app.js */
 
+
+
+/*** Step 2 */
 //  const phrase = new Phrase();
 //  const game = new Game();
 
+
+/*** Step 3 */
 //  const phrase = new Phrase('LIFE is like a box of chocolates');
 //  console.log(`Phrase - phrase: ${phrase.phrase}`);
 
-
+/*** Step 4 */
 // const game = new Game();
 // game.phrases.forEach((phrase, index) => {
 //     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 //     });
 
 
-
+/*** Step 5 */
 // const logPhrase = (phrase) => {
 //     console.log(`Phrase - phrase: `, phrase.phrase);
 // }
@@ -31,11 +36,14 @@
 // logPhrase(game.getRandomPhrase());
 // logPhrase(game.getRandomPhrase());
 
-
+/*** Step 6 */
 // const game = new Game();
 // game.getRandomPhrase().addPhraseToDisplay();
 
+/*** Step 7 */
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+
+/*** Step 8 */
